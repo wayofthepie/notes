0639135d9f8e3360841bb6fc1cfae66bd568a461
wayofthepie <wayofthepie@gmail.com>
2017-09-03 17:53:51 +0100

    Add syntax highlighting
    Also start chapter on regularization

--- deeplearning/default.nix
+++ deeplearning/default.nix
@@ -3,7 +3,13 @@ let
   # Convenience alias for the standard environment
   stdenv = pkgs.stdenv;
   customTexlive = (pkgs.texlive.combine {
-     inherit (pkgs.texlive) scheme-medium pgfplots;
+     inherit (pkgs.texlive)
+      scheme-medium
+      pgfplots
+      minted
+      ifplatform
+      xstring
+      framed;
   });
 in stdenv.mkDerivation {
   name = "deeplearning-book";
@@ -12,6 +18,8 @@ in stdenv.mkDerivation {
   buildInputs = with pkgs; [
     customTexlive
     texmaker
+    python3
+    python3Packages.pygments
   ];
   # This is useful for using this package with --run-env: the PORT environment variable
   installPhase = ''
